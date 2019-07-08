fde9ac6bb95981ab0fd0998e27b6f0ece8a83e6f
0xbunyip <baoduy@autonomous.nyc>
2019-07-08 14:10:43 +0700

    Fix bridge genesis committee
    - Fix hardcoded bridge genesis committee when deploy to ropsten
    - Add struct contracts containing all contract instances
    - Add func to get history of all committees

--- address.go
+++ address.go
@@ -1,5 +1,8 @@
 package bridge
 
-const VaultAddress = "f15e34344ff8582baeff780f1751546971243f0a"
-const TokenAddress = "47e26a86391e382517E2577970E0b9a9C4DA1A8F"
-const IncPaymentAddr = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
+const (
+	IncognitoProxyAddress = "0xAa965CFAf16cEd48478884eb9aBC9eB3B4Af2926" // v5 - enable transfer erc20
+	VaultAddress          = "0xA1b3eE0CaC758F4Ad0cc693Bc02808DA86E86Ffd" // v5 - enable transfer erc20
+	TokenAddress          = "0x47e26a86391e382517E2577970E0b9a9C4DA1A8F" // v2
+	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
+)
