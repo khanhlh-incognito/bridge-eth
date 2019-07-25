69ab101b9497df68c3d081bda48da0460cca1261
0xbunyip <baoduy@autonomous.nyc>
2019-07-25 13:55:52 +0700

    Retest rinkeby

--- address.go
+++ address.go
@@ -1,9 +1,9 @@
 package bridge
 
 const (
-	MulSigP256            = "0x316319D5a2b2e2483A52B382BB30b5aeba0c570f" // v4 - ropsten retest
-	IncognitoProxyAddress = "0x62Daea4d2c7536Fc337A43C81CaC309238b1a8b5" // v13 - ropsten retest
-	VaultAddress          = "0xCF19E9488199f6061619311665151AF43D5A89bf" // v13 - ropsten retest
+	MulSigP256            = "0xe78DC4710861401e65145057F78a52CDF87513fA" // v6 - ropsten retest
+	IncognitoProxyAddress = "0x070228f439AA2aEC154bB0B15CD0011939f66334" // v13 - ropsten retest
+	VaultAddress          = "0xA208F767D423C0aa7DE1498457F177111191c577" // v13 - ropsten retest
 	TokenAddress          = "0x47e26a86391e382517E2577970E0b9a9C4DA1A8F" // v3 - rinkeby
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 )
