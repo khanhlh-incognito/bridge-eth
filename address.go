a4fb943c184806a8b6bffae3e03c10ce705944ea
0xbunyip <baoduy@autonomous.nyc>
2019-07-25 17:45:35 +0700

    Update to contract v15: ropsten without mulsig

--- address.go
+++ address.go
@@ -1,9 +1,9 @@
 package bridge
 
 const (
-	MulSigP256            = "0xe78DC4710861401e65145057F78a52CDF87513fA" // v6 - ropsten retest
-	IncognitoProxyAddress = "0x7b78E6829bDD5951aA2C512f2cDa4E551000FEc2" // v14 - rinkeby without calling mulsig
-	VaultAddress          = "0xB2eCC6ca4Ee97D1174adA94b63BAcfd8ea96B9e6" // v14 - rinkeby without calling mulsig
+	MulSigP256            = "0x1383aa86ac1f7bf638baad2311448a40493f691f" // v1
+	IncognitoProxyAddress = "0x32521FF6d5F61acA44B2F10189f1770358cf5D88" // v15 - ropsten without calling mulsig
+	VaultAddress          = "0xA80cB16fCd5BbbE8E9a9Fb247eAa2B87D9488556" // v15 - ropsten without calling mulsig
 	TokenAddress          = "0x47e26a86391e382517E2577970E0b9a9C4DA1A8F" // v3 - rinkeby
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 )
