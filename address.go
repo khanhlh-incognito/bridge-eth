5f8a8137edbd37e50af21e7db75659f6a870c76e
0xbunyip <baoduy@autonomous.nyc>
2019-08-08 18:51:40 +0700

    New token on Kovan: Phuong (DE)

--- address.go
+++ address.go
@@ -4,6 +4,6 @@ const (
 	MulSigP256            = "0x1383aa86ac1f7bf638baad2311448a40493f691f" // v4 - dummy address on Kovan
 	IncognitoProxyAddress = "0x11a6Bca5FCb787Fe5e5CBC0a89AB9Eb23692067F" // v17 - kovan with numVals and without verify pubkeys
 	VaultAddress          = "0xe97f7d3F866CB8200941082c887c7aE5eEAB1f58" // v17
-	TokenAddress          = "0xEee71a273b51a79EE51EA3818DabcF8477cD627b" // v5 - kovan new token for dev/master
+	TokenAddress          = "0xB2eCC6ca4Ee97D1174adA94b63BAcfd8ea96B9e6" // v6 - kovan new token Phuong (DE)
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 )
