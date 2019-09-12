c5b97d90aa0e6e283ad533a6eb346bca32d24655
0xbunyip <>
2019-09-12 14:01:02 +0700

    Deploy contract v20 for dev-testnet

--- address.go
+++ address.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	IncognitoProxyAddress = "0x457213836AA7eA16003f0529CeF99ADFcffD0D61" // v19 - solidity, for testnet
-	VaultAddress          = "0xaB4d7995bBfe53e86EAAF365c473B443C5320527" // v19 - solidity, for testnet
+	IncognitoProxyAddress = "0x00504c1390fA42B8Da27Cf75Ca888cBb97809aE0" // v20 - solidity, kovan, for dev-testnet
+	VaultAddress          = "0x143ac3529245ba1dF71Ff8876981aEC3aCE11784" // v20 - solidity, kovan, for dev-testnet
 	TokenAddress          = "0x1d59ee76304338fac3a0ea9ae06e618c760d6042" // v7 - kovan token Chicken
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 )
