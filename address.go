de3b7501d2b26b99d75176945d01fd69ce404b00
0xbunyip <>
2019-10-21 08:31:28 +0700

    Contract v33

--- address.go
+++ address.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	IncognitoProxyAddress = "0x880449aC4381AcD6fA8c1152179D918e80E6c02C" // v31 - solidity, kovan, dev-testnet, for new-privacy-dev
-	VaultAddress          = "0x717B5F3667A21a0b5e09A8d0E8648C1D525503C4" // v31 - solidity, kovan, dev-testnet, for new-privacy-dev
+	IncognitoProxyAddress = "0xd0e05fA2F3935eCd35B36914e0C72fA0DDD34238" // v33 - solidity, kovan, testnet, for new-privacy-dev
+	VaultAddress          = "0xE7A65fC751BF3D981a6423fD6F203cb45CCA9405" // v33 - solidity, kovan, testnet, for new-privacy-dev
 	TokenAddress          = "0x1d59ee76304338fac3a0ea9ae06e618c760d6042" // v7 - kovan token Chicken
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 )
