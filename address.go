8f6dca019321218a3640a38c10e0cc0f7fa469d9
0xbunyip <>
2019-10-29 15:41:52 +0700

    Contract v37: mainnet

--- address.go
+++ address.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	IncognitoProxyAddress = "0xf97D4Ac44Cb46EE9EA426C81691422F199119dB5" // v36 - kovan, testnet, for branch master-temp-B, with admin
-	VaultAddress          = "0x63837136B048F4F07fAFA5e6b10b92103df64A4E" // v36 - kovan, testnet, for branch master-temp-B, with admin
+	IncognitoProxyAddress = "0xE34ebFf7aF293dcdDf0443818442BB5d558662Dd" // v37 - mainnet, for branch master-temp-B, with admin, 7+22 validators
+	VaultAddress          = "0xd8f7cac14156eb1d1EA1a28cd04D23cE0f142602" // v37 - mainnet, for branch master-temp-B, with admin, 7+22 validators
 	TokenAddress          = "0x1d59ee76304338fac3a0ea9ae06e618c760d6042" // v7 - kovan token Chicken
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 	Admin                 = "0x037ac7fFfC1C52Cf6351e33A77eDBdd14CE35040"
