bfcd54de64bb8d55788714f8255a479c2515b4cc
0xbunyip <>
2019-11-11 01:36:35 +0700

    Contract v1-dev: test migrate

--- address.go
+++ address.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	IncognitoProxyAddress = "0x9bEb9F876fc08f92c270609dc51f215F9EeA973d" // v3.t - kovan, decimals > 18, fixed withdraw
-	VaultAddress          = "0x83F5697F0e6F57edd92c569e224d0824C42161A0" // v3.t - kovan, decimals > 18, fixed withdraw
+	IncognitoProxyAddress = "0x4395DDB1c1E036Af17F91d5eDFeED6ce05FAAe23" // v1-dev - kovan, test migrate
+	VaultAddress          = "0xecbF13E35202417F14ef0b21f0F0005617212EA5" // v1-dev - kovan, test migrate
 	TokenAddress          = "0x1d59ee76304338fac3a0ea9ae06e618c760d6042" // v7 - kovan token Chicken
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 	Admin                 = "0x65033F315F214834BD6A65Dce687Bcb0f32b0a5A"
