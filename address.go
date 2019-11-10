a52e746cddb58500a0352a3cab5329b6ae1f4737
0xbunyip <>
2019-11-10 23:45:42 +0700

    Contract v3.t

--- address.go
+++ address.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	IncognitoProxyAddress = "0x56BD5B268bB26F1f118790fF31FEBD5596EEa501" // v2.t - kovan, decimals > 18
-	VaultAddress          = "0x883C01E2b6568Da2463fc7a8386Bd736f2b786DA" // v2.t - kovan, decimals > 18
+	IncognitoProxyAddress = "0x9bEb9F876fc08f92c270609dc51f215F9EeA973d" // v3.t - kovan, decimals > 18, fixed withdraw
+	VaultAddress          = "0x83F5697F0e6F57edd92c569e224d0824C42161A0" // v3.t - kovan, decimals > 18, fixed withdraw
 	TokenAddress          = "0x1d59ee76304338fac3a0ea9ae06e618c760d6042" // v7 - kovan token Chicken
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 	Admin                 = "0x65033F315F214834BD6A65Dce687Bcb0f32b0a5A"
