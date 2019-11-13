d8ce5be39720f213772c36304b22cf7b561decbe
0xbunyip <>
2019-11-13 15:48:26 +0700

    Add DAI and deploy to testnet

--- address.go
+++ address.go
@@ -3,7 +3,7 @@ package main
 const (
 	IncognitoProxyAddress = "0x8276043e1B426E10D2BA8B2773C074c60560840d" // v3-dev - kovan, test migrate, with fallback
 	VaultAddress          = "0xB2e10F9a0CD87151540D36a4fac7f0FA23CAB9E4" // v4-dev - kovan, test migrate, with fallback, point to prev vault v3-dev
-	TokenAddress          = "0x236E18f09D0F975d412D87EE3536D3037b344a2E" // v8 - kovan token GUITAR, decimals=30
+	TokenAddress          = "0xB698a7d50AC03448867584959391BD60f56C2252" // v10 - kovan token DAI
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 	Admin                 = "0x65033F315F214834BD6A65Dce687Bcb0f32b0a5A"
 	Successor             = "0x65033F315F214834BD6A65Dce687Bcb0f32b0a5A"
---
+++ customerc20_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/incognitochain/bridge-eth/erc20/dai"
+)
+
+func TestDAIDeploy(t *testing.T) {
+	privKey, client, err := connect()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	// Deploy DAI
+	auth := bind.NewKeyedTransactor(privKey)
+	symbol := [32]byte{'D', 'A', 'I'}
+	addr, tx, d, err := dai.DeployDai(auth, client, symbol)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("deployed dai")
+	fmt.Printf("addr: %s\n", addr.Hex())
+
+	// Wait till tx is done
+	err = wait(client, tx.Hash())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Mint DAI
+	bal, _ := big.NewInt(1).SetString("1000000000000000000000000000", 10)
+	tx, err = d.Mint(auth, bal)
+
+	// daiABI, _ := abi.JSON(strings.NewReader(dai.DaiABI))
+	// fmt.Printf("%+v\n", daiABI.Methods)
+	// fmt.Printf("%+v\n", daiABI.Methods["mint"])
+	// contract := bind.NewBoundContract(address, daiABI, client, client, client)
+	// dtr := dai.DaiTransactor{contract: contract}
+	// tx, err = dtr.Mint(auth, crypto.PubkeyToAddress(privKey.PublicKey), bal)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("minted dai, tx: %s\n", tx.Hash().Hex())
+}
