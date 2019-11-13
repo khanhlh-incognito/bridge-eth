4ca181f9b68ddece3295268564f8d9200b4902d0
0xbunyip <>
2019-11-13 13:02:09 +0700

    Revert Proxy and Vault address to testnet

--- address.go
+++ address.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	IncognitoProxyAddress = "0x8276043e1B426E10D2BA8B2773C074c60560840d" // v3-dev - kovan, test migrate, with fallback
-	VaultAddress          = "0xB2e10F9a0CD87151540D36a4fac7f0FA23CAB9E4" // v4-dev - kovan, test migrate, with fallback, point to prev vault v3-dev
+	IncognitoProxyAddress = "0x9bEb9F876fc08f92c270609dc51f215F9EeA973d" // v3.t - kovan, decimals > 18, fixed withdraw
+	VaultAddress          = "0x83F5697F0e6F57edd92c569e224d0824C42161A0" // v3.t - kovan, decimals > 18, fixed withdraw
 	TokenAddress          = "0x236E18f09D0F975d412D87EE3536D3037b344a2E" // v8 - kovan token GUITAR, decimals=30
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 	Admin                 = "0x65033F315F214834BD6A65Dce687Bcb0f32b0a5A"
