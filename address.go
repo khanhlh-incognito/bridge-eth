afe39c85dda72481f0c2dd6748a892c5b1f0c05d
0xbunyip <baoduy@autonomous.nyc>
2019-11-29 10:56:45 +0700

    Deploy contract v2-test

--- address.go
+++ address.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	IncognitoProxyAddress = "0x86a5b7B9993f33e5EDA898f9c6c5f0C5863a6D38" // v1-test - kovan, fixed transfer() and decimals()
-	VaultAddress          = "0x99A93076Dc2aBE9A34dddC6dAaF6608d35Ecc86c" // v1-test - kovan, fixed transfer() and decimals()
+	IncognitoProxyAddress = "0x6b8B83235f20875A797571f7bC0bD7dD54205213" // v2-test - kovan, fixed transfer() and decimals()
+	VaultAddress          = "0x6e8CDB333ba1573Fffe195A545F3031Cff9Da008" // v2-test - kovan, fixed transfer() and decimals()
 	TokenAddress          = "0xB698a7d50AC03448867584959391BD60f56C2252" // v10 - kovan token DAI
 	IncPaymentAddr        = "1Uv46Pu4pqBvxCcPw7MXhHfiAD5Rmi2xgEE7XB6eQurFAt4vSYvfyGn3uMMB1xnXDq9nRTPeiAZv5gRFCBDroRNsXJF1sxPSjNQtivuHk"
 	Admin                 = "0x65033F315F214834BD6A65Dce687Bcb0f32b0a5A"
